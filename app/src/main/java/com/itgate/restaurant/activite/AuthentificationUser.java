119bf37986f2b014ed117022bd4326634e396f22
macbook <angoudhoussem@gmail.com>
2016-04-24 17:48:20 +0100

    last commit

--- app/src/main/java/com/itgate/restaurant/activite/AuthentificationUser.java
+++ app/src/main/java/com/itgate/restaurant/activite/AuthentificationUser.java
@@ -2,6 +2,9 @@ package com.itgate.restaurant.activite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.itgate.restaurant.R;
 
@@ -11,5 +14,12 @@ public class AuthentificationUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentification_user);
+        Button  btnlogin= (Button) findViewById(R.id.button);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"clic me",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
